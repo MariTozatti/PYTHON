3c1ea7c18952af683db115e5ffab8aa0422a56d4
Mariana <tozattimariana@gmail.com>
2025-07-26 22:28:22 -0300

    Create EX01.py
    Documentação do que aprendi nas aulas "primeiros passos com Python", deixei alguns comentários sobre o que aprendi no código.

---
+++ EX01.py
@@ -0,0 +1,21 @@
+#1° exercício de python do Curso em Vídeo
+print ('olá, mundo!')
+nome = input ('Qual seu nome?')
+print ('Olá,' , nome)
+
+"""
+OU
+nome = input ('Qual seu nome?')
+print ('Olá, {}'.format(nome))
+o format substitui o {} - não sei muito bem a diferença
+"""
+
+"""
+OBS:
+('') - MOSTRA UMA MENSAGEM 
+() - MOSTRA O NÚMERO OU A CONTA +-*/
+PARA PYTHON TODOS OS COMANDOS SÃO FUNÇÕES E TODAS VARIAVEIS SÃO OBJETOS
+CONCATENAR - + OU , O MAIS SÓ FUNCIOA COM STRING
+FIM DO PROGRAMA - LINHA VAZIA
+"""
+
