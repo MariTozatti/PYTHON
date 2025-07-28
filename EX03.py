a0fdc0bd5baadb1cc8736a6739d4f60d506898a1
Mariana <tozattimariana@gmail.com>
2025-07-28 12:48:57 -0300

    Create EX03.py
    Documentação do que aprendi sobre o vídeo de Operadores Aritimeticos

---
+++ EX03.py
@@ -0,0 +1,16 @@
+"""
+Operadores Aritiméticos
++ - adição
+- - subtração 
+* - multiplicação
+/ - divisão
+** - potencia
+// - divisão inteira
+% - resto da divisão
+
+Ordem 
+() | ** | * / // % | + -
+
+não se uma {} nem [], eles tem outra funções no python
+"""
+
