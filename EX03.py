df5000bc87b96ea122159ad6ec05c94994fc84a3
Mariana <tozattimariana@gmail.com>
2025-07-28 21:32:26 -0300

    Update EX03.py
    Documentação do que aprendi no vídeo sobre Operadores Aritimeticos

--- EX03.py
+++ EX03.py
@@ -14,3 +14,16 @@ Ordem
 não se uma {} nem [], eles tem outra funções no python
 """
 
+nome = input('Qual seu nome?')
+print('Prazer em \n te conhecer {:10}!'.format(nome), end= '')
+print ('Ola')
+# ao inves de aparecer duas linas, o end junta os dois prints
+# \n pula linha
+#:20 - vai aparecer assim mariana   ! (10 caracteres)
+#:>10 - vai alinhar a direita    mariana!
+#:<10 - vai alinhar a esquerda mariana   !
+#:^10 - vai centralizar   mariana   !
+#:=^10 - vai centralizar ==mariana==! (qualuqe sinal)
+
+#caso uma divisão de 1.33333333, usar {:.3f}(e casa e float) - 1.333
+
